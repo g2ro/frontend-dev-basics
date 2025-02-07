24d7c4038a0222fe85c4f7ea580791cfb7a37891
g2ro <kilho603@gmail.com>
2025-02-07 10:11:18 +0900

    [es6 features/ex08] node export, import

--- javascript-practices/es6 features/ex08/index.mjs
+++ javascript-practices/es6 features/ex08/index.mjs
@@ -1,4 +1,23 @@
 console.log('===[Test: Unnamed Export Module]'); // Unnamed Export Module Import 에서은 반드시 이름을 지정해야 한다.
-console.log('===[Test: Named Export Module I]'); // Named Export 모듈의 다수 객체 Destrcting Import
-console.log('===[Test: Named Export Module II]'); // Named Export Module은 다수의 객체를 Export 하기 때문에 Import 할 때 특정 이름을 지정할 수 없다. 대신, '* as 이름' 를 사용할 수 있다.
+import myFunction from './mods/ex01.mjs';
+console.log(myFunction(10, 20));
+
+import myObject from './mods/ex02.mjs'; // 관례상 import를 맨 앞으로 넣는다 => 문법적으론 import 를 중간에 넣을 수 있지만, 실행이 import로 인해 잠깐 멈춤이 발생할 수 있고, 이는 성능저하를 발생시킨다.
+console.log(myObject.add(10, 20));
+console.log(myObject.substract(20, 10));
+
+console.log('===[Test: named Export Module II]'); // Named Export 모듈의 다수 객체 Destrcting Import
+import {substract} from './mods/ex03.mjs';
+console.log(substract(10, 20));
+
+console.log('===[Test: Named Export Module ]'); // Named Export Module은 다수의 객체를 Export 하기 때문에 Import 할 때 특정 이름을 지정할 수 없다. 대신, '* as 이름' 를 사용할 수 있다.
+import * as m from './mods/ex03.mjs'; // name export시 모든 객체를 받고 싶다면 as 이름으로 묶어서 사용한다.
+console.log(m.add(10,20));
+console.log(m.substract(10,20))
+
+
 console.log('===[Test: Named + Unamed Export Module]')
+import math, {add} from './mods/ex04.mjs';
+
+console.log(math.add(10, 20));
+console.log(add(10, 20));
--- javascript-practices/es6 features/ex08/mods/ex02.mjs
+++ javascript-practices/es6 features/ex08/mods/ex02.mjs
@@ -5,3 +5,22 @@
     2. 주의: 이름 없이 export 되기 때문에 하나만 export 할 수 있다.
 
  */
+
+// 방법 1
+// export default{
+//     add: (a, b) => a + b,
+//     substract: (a, b) => a - b
+// }
+
+// 방법 2
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+
+export default {add, substract};
+
+// 위의 export는 아래의 export 문을 축약한 버전이다.
+
+// export default{
+//     add: add,
+//     substract: substract
+// }
