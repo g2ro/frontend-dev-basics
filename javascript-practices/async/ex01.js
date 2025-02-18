7741a79095a56136fbf26003eaf3ff1f32f5f470
g2ro <kilho603@gmail.com>
2025-02-18 09:22:54 +0900

    async #finish

--- javascript-practices/async/ex01.js
+++ javascript-practices/async/ex01.js
@@ -1,4 +1,25 @@
 /*
     callback 지원 비동기 함수 사용
 */
+const {fn01} = require('./fns');
 
+// test: success
+fn01('param~~', function(error, result) {
+    if(error) {
+       console.error(error);
+       return; 
+    }
+    console.log('result:' + result);
+});
+
+// test: fail
+fn01(null, function(error, result) {
+    if(error) {
+       console.error(error);
+       return; 
+    }
+    console.log('result:' + result);
+});
+
+
+console.log('wait....');
